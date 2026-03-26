fe796f011c5829b858baf8805d37525c89a635ed
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-03-26 06:31:17 -0400

    Fixed test case self method check

--- grunt-testcase/src/main/java/net/spartanb312/grunteon/testcase/methodrename/OverlapInterface2To1.java
+++ grunt-testcase/src/main/java/net/spartanb312/grunteon/testcase/methodrename/OverlapInterface2To1.java
@@ -1,5 +1,7 @@
 package net.spartanb312.grunteon.testcase.methodrename;
 
+import net.spartanb312.grunteon.testcase.ParameterMarkerDummy;
+
 import java.util.Random;
 
 import static net.spartanb312.grunteon.testcase.Asserts.assertEquals;
@@ -7,39 +9,38 @@ import static net.spartanb312.grunteon.testcase.Asserts.assertTrue;
 
 public class OverlapInterface2To1 {
     private static void checkFather1(Object o) {
-        assertEquals(42, ((Father1) o).foo());
+        assertEquals(42, ((Father1) o).foo(ParameterMarkerDummy.INSTANCE));
     }
 
     private static void checkFather2(Object o) {
-        assertEquals(42, ((Father2) o).foo());
+        assertEquals(42, ((Father2) o).foo(ParameterMarkerDummy.INSTANCE));
     }
-
 
     public static void main(String[] args) throws NoSuchMethodException {
         Child child = new Child();
-        assertEquals(42, child.foo());
+        assertEquals(42, child.foo(ParameterMarkerDummy.INSTANCE));
         Object o = new Random().nextLong(0, 1) < 114 ? child : new Object(); // Fake random to prevent inlining
 
         checkFather1(o);
         checkFather2(o);
 
-        String n1 = Father1.class.getMethods()[0].getName();
-        String n2 = Father2.class.getMethods()[0].getName();
-        String n3 = Child.class.getMethods()[0].getName();
+        String n1 = ParameterMarkerDummy.findFirstMethodWithMarker(Father1.class).getName();
+        String n2 = ParameterMarkerDummy.findFirstMethodWithMarker(Father2.class).getName();
+        String n3 = ParameterMarkerDummy.findFirstMethodWithMarker(Child.class).getName();
         assertTrue(n1.equals(n2) && n2.equals(n3), "Test failed: Method names do not match: " + String.join(", ", n1, n2, n3));
     }
 
     private interface Father1 {
-        int foo();
+        int foo(ParameterMarkerDummy dummy);
     }
 
     private interface Father2 {
-        int foo();
+        int foo(ParameterMarkerDummy dummy);
     }
 
     private static class Child implements Father1, Father2 {
         @Override
-        public int foo() {
+        public int foo(ParameterMarkerDummy dummy) {
             return 42;
         }
     }
--- grunt-testcase/src/main/java/net/spartanb312/grunteon/testcase/methodrename/OverloadShadow1.java
+++ grunt-testcase/src/main/java/net/spartanb312/grunteon/testcase/methodrename/OverloadShadow1.java
@@ -1,5 +1,7 @@
 package net.spartanb312.grunteon.testcase.methodrename;
 
+import net.spartanb312.grunteon.testcase.ParameterMarkerDummy;
+
 import java.util.Arrays;
 
 import static net.spartanb312.grunteon.testcase.Asserts.assertEquals;
@@ -7,21 +9,21 @@ import static net.spartanb312.grunteon.testcase.Asserts.assertNotEquals;
 
 public class OverloadShadow1 {
     private static class C1 {
-        public int a(int a) {
+        public int a(ParameterMarkerDummy dummy, int a) {
             return a + 1;
         }
 
-        public int b(int a, int b) {
+        public int b(ParameterMarkerDummy dummy, int a, int b) {
             return a + b + 11;
         }
     }
 
     private static class C2 extends C1 {
-        public int c(int a, int b) {
+        public int c(ParameterMarkerDummy dummy, int a, int b) {
             return a * b + 14;
         }
 
-        public int d(int a, int b, int c) {
+        public int d(ParameterMarkerDummy dummy, int a, int b, int c) {
             return a * b + 14 + c;
         }
     }
@@ -29,17 +31,17 @@ public class OverloadShadow1 {
     @SuppressWarnings("OptionalGetWithoutIsPresent")
     public static void main(String[] args) {
         C1 c1 = new C1();
-        assertEquals(69, c1.a(68));
-        assertEquals(25, c1.b(8, 6));
+        assertEquals(69, c1.a(ParameterMarkerDummy.INSTANCE, 68));
+        assertEquals(25, c1.b(ParameterMarkerDummy.INSTANCE, 8, 6));
 
         C2 c2 = new C2();
-        assertEquals(69, c2.a(68));
-        assertEquals(25, c2.b(8, 6));
-        assertEquals(94, c2.c(1, 80));
-        assertEquals(129, c2.d(20, 5, 15));
+        assertEquals(69, c2.a(ParameterMarkerDummy.INSTANCE, 68));
+        assertEquals(25, c2.b(ParameterMarkerDummy.INSTANCE, 8, 6));
+        assertEquals(94, c2.c(ParameterMarkerDummy.INSTANCE, 1, 80));
+        assertEquals(129, c2.d(ParameterMarkerDummy.INSTANCE, 20, 5, 15));
 
-        String c1n1 = C1.class.getMethods()[0].getName();
-        String c1n2 = C1.class.getMethods()[1].getName();
+        String c1n1 = ParameterMarkerDummy.findMethodsWithMarker(C1.class).filter(e -> e.getParameterCount() == 2).findAny().get().getName();
+        String c1n2 = ParameterMarkerDummy.findMethodsWithMarker(C1.class).filter(e -> e.getParameterCount() == 3).findAny().get().getName();
         assertEquals(c1n1, c1n2, "Method names in Child1 not equal");
 
         String c2n1 = Arrays.stream(C2.class.getMethods()).filter(e -> e.getParameterCount() == 2).findAny().get().getName();
