36c549d82dd2d43ecf1332b9dbd86ec19f72eb8f
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-03-26 03:55:29 -0400

    MethodHierarchy benchmark

--- grunt-main/src/main/kotlin/net/spartanb312/grunteon/obfuscator/process/hierarchy2/MethodHierarchy.kt
+++ grunt-main/src/main/kotlin/net/spartanb312/grunteon/obfuscator/process/hierarchy2/MethodHierarchy.kt
@@ -9,6 +9,7 @@ import net.spartanb312.genesis.kotlin.extensions.isPrivate
 import net.spartanb312.grunteon.obfuscator.Grunteon
 import net.spartanb312.grunteon.obfuscator.config.manager.ConfigGroup
 import net.spartanb312.grunteon.obfuscator.pipeline.ProcessPipeline
+import net.spartanb312.grunteon.obfuscator.process.hierarchy.HeavyHierarchy
 import net.spartanb312.grunteon.obfuscator.util.extensions.isInitializer
 import org.objectweb.asm.tree.MethodNode
 import java.util.*
@@ -201,15 +202,41 @@ fun main() {
     val instance = Grunteon(ConfigGroup(), ProcessPipeline())
     instance.init()
 
-    measureTime {
-        MethodHierarchy.build(ClassHierarchy.build(instance.allClasses, instance.workRes::getClassNode))
-    }.also { println("Cold: %.2f ms".format(it.toDouble(DurationUnit.MILLISECONDS))) }
-//    repeat(20) {
-//        MethodHierarchy.build(ClassHierarchy.build(instance.allClasses, instance.workRes::getClassNode))
-//    }
-//    measureTime {
-//        repeat(5) {
-//            MethodHierarchy.build(ClassHierarchy.build(instance.allClasses, instance.workRes::getClassNode))
-//        }
-//    }.also { println("%.2f ms".format(it.toDouble(DurationUnit.MILLISECONDS) / 5.0)) }
+    val old = false
+
+    if (old) {
+        measureTime {
+            HeavyHierarchy(instance).apply {
+                buildClass()
+                buildMethod()
+            }
+        }.also { println("Old Cold: %.2f ms".format(it.toDouble(DurationUnit.MILLISECONDS))) }
+
+        repeat(20) {
+            HeavyHierarchy(instance).apply {
+                buildClass()
+                buildMethod()
+            }
+        }
+        measureTime {
+            repeat(5) {
+                HeavyHierarchy(instance).apply {
+                    buildClass()
+                    buildMethod()
+                }
+            }
+        }.also { println("Old %.2f ms".format(it.toDouble(DurationUnit.MILLISECONDS) / 5.0)) }
+    } else {
+        measureTime {
+            MethodHierarchy.build(ClassHierarchy.build(instance.allClasses, instance.workRes::getClassNode))
+        }.also { println("New Cold: %.2f ms".format(it.toDouble(DurationUnit.MILLISECONDS))) }
+        repeat(20) {
+            MethodHierarchy.build(ClassHierarchy.build(instance.allClasses, instance.workRes::getClassNode))
+        }
+        measureTime {
+            repeat(5) {
+                MethodHierarchy.build(ClassHierarchy.build(instance.allClasses, instance.workRes::getClassNode))
+            }
+        }.also { println("New %.2f ms".format(it.toDouble(DurationUnit.MILLISECONDS) / 5.0)) }
+    }
 }
