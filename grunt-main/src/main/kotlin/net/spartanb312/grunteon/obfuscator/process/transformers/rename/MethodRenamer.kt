10f79cd44f0f596a3e60e8b3ef4ed8cf731a79c5
NoobSex <61795895+SpartanB312@users.noreply.github.com>
2026-03-30 09:04:03 +0800

    Group discard

--- grunt-main/src/main/kotlin/net/spartanb312/grunteon/obfuscator/process/transformers/rename/MethodRenamer.kt
+++ grunt-main/src/main/kotlin/net/spartanb312/grunteon/obfuscator/process/transformers/rename/MethodRenamer.kt
@@ -11,6 +11,7 @@ import net.spartanb312.grunteon.obfuscator.util.IndyChecker
 import net.spartanb312.grunteon.obfuscator.util.Logger
 import net.spartanb312.grunteon.obfuscator.util.extensions.*
 import net.spartanb312.grunteon.obfuscator.util.interfaces.DisplayEnum
+import org.objectweb.asm.tree.ClassNode
 
 /**
  * Renaming methods
@@ -99,54 +100,55 @@ class MethodRenamer : Transformer<MethodRenamer.Config>(
         seq {
             val methodHierarchy = methodHierarchy.global
             val classHierarchy = methodHierarchy.classHierarchy
-            context(classHierarchy, methodHierarchy) {
-                Logger.info("    Splitting method groups...")
-                val strategy = buildFilterStrategy(config)
-                val blackList = mutableSetOf<MethodHierarchy.Entry>()
-                val relatedGroups = mutableListOf<MutableSet<MethodHierarchy.Entry>>() // as a family
-                instance.workRes.inputClassCollection.asSequence()
-                    .filter {
-                        strategy.testClass(it)
+            val strategy = buildFilterStrategy(config)
+            val nonExcluded: List<ClassNode> = instance.workRes.inputClassCollection
+                .filter {
+                    strategy.testClass(it)
                             && !it.isAnnotation
                             && (config.enums || !it.isEnum)
                             && (config.interfaces || !it.isInterface)
-                    }.forEach { classNode ->
-                        val classIndex = classHierarchy.findClass(classNode.name)
-                        if (classIndex == -1) throw Exception("你妈${classNode.name}死了，hierarchy里面找不到你妈")
-                        val classEntry = ClassHierarchy.Entry(classIndex)
-                        if (!classEntry.hasMissingDependency) {
-                            val isEnum = classNode.isEnum
-                            for (methodIndex in classEntry.methods.indices) {
-                                val methodEntry = MethodHierarchy.Entry(methodIndex)
-                                // Source check
-                                if (!methodEntry.isSourceMethod) continue
-                                if (blackList.contains(methodEntry)) continue
-                                // Info check
-                                val methodNode = methodEntry.node
-                                if (methodNode.isNative) continue
-                                if (methodNode.isInitializer) continue
-                                if (methodNode.isMainMethod) continue
-                                if (isEnum && methodNode.name == "values") continue
-                                // if (methodNode.reflectionExcluded) continue
-                                // TODO: method exclusion
-                                // val combined = combine(classNode.name, methodNode.name, methodNode.desc)
-                                // Bind group
-                                val related = methodEntry.connectedComponent
-                                val group = if (related.size > 1) {
-                                    val relationship = related.indices.map { MethodHierarchy.Entry(it) }.toMutableSet()
-                                    relationship.add(methodEntry) // idk
-                                    Logger.info("    Found multi source method group: ")
-                                    relationship.forEach {
-                                        Logger.info("     - " + it.full)
-                                    }
-                                    relationship
-                                } else mutableSetOf(methodEntry)
-                                // apply group
-                                blackList.addAll(group)
-                                relatedGroups.add(group)
-                            }
+                }
+            context(classHierarchy, methodHierarchy) {
+                Logger.info("    Splitting method groups...")
+                val blackList = mutableSetOf<MethodHierarchy.Entry>()
+                val relatedGroups = mutableListOf<MutableSet<MethodHierarchy.Entry>>() // as a family
+                nonExcluded.forEach { classNode ->
+                    val classIndex = classHierarchy.findClass(classNode.name)
+                    if (classIndex == -1) throw Exception("你妈${classNode.name}死了，hierarchy里面找不到你妈")
+                    val classEntry = ClassHierarchy.Entry(classIndex)
+                    if (!classEntry.hasMissingDependency) {
+                        val isEnum = classNode.isEnum
+                        for (methodIndex in classEntry.methods.indices) {
+                            val methodEntry = MethodHierarchy.Entry(methodIndex)
+                            // Source check
+                            if (!methodEntry.isSourceMethod) continue
+                            if (blackList.contains(methodEntry)) continue
+                            // Info check
+                            val methodNode = methodEntry.node
+                            if (methodNode.isNative) continue
+                            if (methodNode.isInitializer) continue
+                            if (methodNode.isMainMethod) continue
+                            if (isEnum && methodNode.name == "values") continue
+                            // if (methodNode.reflectionExcluded) continue
+                            // TODO: method exclusion
+                            // val combined = combine(classNode.name, methodNode.name, methodNode.desc)
+                            // Bind group
+                            val related = methodEntry.connectedComponent
+                            val group = if (related.size > 1) {
+                                val relationship = related.indices.map { MethodHierarchy.Entry(it) }.toMutableSet()
+                                relationship.add(methodEntry) // idk
+                                Logger.info("    Found multi source method group: ")
+                                relationship.forEach {
+                                    Logger.info("     - " + it.full)
+                                }
+                                relationship
+                            } else mutableSetOf(methodEntry)
+                            // apply group
+                            blackList.addAll(group)
+                            relatedGroups.add(group)
                         }
                     }
+                }
 
                 Logger.info("    Generating mappings for method groups...")
                 val dictionary = NameGenerator.getDictionary(config.dictionary)
@@ -166,12 +168,20 @@ class MethodRenamer : Transformer<MethodRenamer.Config>(
                     val checkList = mutableSetOf<Pair<ClassHierarchy.Entry, String>>()
                     group.forEach { source ->
                         checkList.add(source.owner to source.full)
-                        // TODO: check this
                         val descendants = source.owner.descendants.map {
                             val ownerName = classHierarchy.classNames[it]
                             ClassHierarchy.Entry(it) to "${ownerName}.${source.name}${source.desc}"
                         }
                         checkList.addAll(descendants)
+                    }
+                    for ((owner: ClassHierarchy.Entry, _) in checkList) {
+                        if (!nonExcluded.contains(owner.classNode)) {
+                            Logger.error("${owner.classNode.name} is not included in working range. Discarded all group")
+                            checkList.forEach {
+                                Logger.info(" - ${it.first.name}")
+                            }
+                            return@forEach
+                        }
                     }
                     var newName: String
                     loop@ while (true) {
